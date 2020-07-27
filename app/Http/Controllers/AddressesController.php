7b0a915ea036745939ff92e95b9677ba6d167598
TopEngineer <eric.leezone@gmail.com>
2020-07-28 04:17:02 +0900

    Update Template

--- app/Http/Controllers/AddressesController.php
+++ app/Http/Controllers/AddressesController.php
@@ -143,7 +143,7 @@ class AddressesController extends Controller
             $message->replyTo('davidlimdev@gmail.com', 'David Lim');
             $message->to($address->email);
             $message->subject($template->subject);
-            $message->setBody('Hi ' . $address->name . '. \n' . $template->content, 'text/html');
+            $message->setBody('Hi ' . $address->name . '. \r\n' . $template->content);
         });
         $request->session()->flash('message', 'Successfully sent e-mail');
         return redirect()->route('addresses.index');
